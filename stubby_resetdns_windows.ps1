e5123e232ee2120b0887614aab7665a27d76dc70
unhealme <o2tongpethuk@gmail.com>
2020-05-14 10:27:56 +0700

    first commit

---
+++ stubby_resetdns_windows.ps1
@@ -0,0 +1,16 @@
+﻿$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
+}
+Get-NetAdapter -Physical | ForEach-Object {
+   $ifname = $_.Name
+   Write-Host "Resetting DNS servers on interface $ifname - the system will use default DNS service."
+   set-dnsclientserveraddress $ifname -ResetServerAddresses
+   $new_value = get-dnsclientserveraddress $ifname
+   Write-Output -InputObjext $new_value
+}
+$p = Get-Process -Name "stubby"
+Stop-Process -InputObject $p
+Get-Process | Where-Object {$_.HasExited}
---
+++ stubby_setdns_windows.ps1
@@ -0,0 +1,14 @@
+﻿$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
+}
+Get-NetAdapter -Physical | ForEach-Object {
+   $ifname = $_.Name
+   Write-Host "Setting DNS servers on interface $ifname to use local resolver - the system will use Stubby if it is running"
+   set-dnsclientserveraddress $ifname -ServerAddresses ("127.0.0.1","0::1")
+   $new_value = get-dnsclientserveraddress $ifname
+   Write-Output -InputObjext $new_value
+}
+Start-Process -FilePath "C:\Program Files\Stubby\stubby.exe" -WindowStyle Hidden
